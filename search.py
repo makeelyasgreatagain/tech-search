7a45805e94178c000fbe7e3a4ba278f920ca7a8b
Elyas <elysignup@gmail.com>
2025-09-19 01:42:21 +0330

    Update search.py
    Now it extracts titles and links

--- search.py
+++ search.py
@@ -1,8 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
+
 
 search_term = input("Enter your search term: ")
 
 response = requests.get("https://digiato.com/", params={"s": search_term})
 
-if response.status_code == 200:
-    print(response.text)
+soup = BeautifulSoup(response.text, "html.parser")
+
+titles = soup.find_all("a", class_="rowCard__title")
+
+for title in titles:
+    print(f"Title: {title.get_text(strip=True)}")
+    print(f"URL: {title.get("href")}\n")
